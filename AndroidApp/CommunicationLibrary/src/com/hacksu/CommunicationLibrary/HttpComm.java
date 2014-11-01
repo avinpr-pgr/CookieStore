57973d0c871103c8ea14755ec13950979da309de
Mohammad Doleh <mdoleh@kent.edu>
2014-10-31 22:02:45 -0400

    Comm & Product Classes

--- AndroidApp/CommunicationLibrary/src/com/hacksu/CommunicationLibrary/HttpComm.java
+++ AndroidApp/CommunicationLibrary/src/com/hacksu/CommunicationLibrary/HttpComm.java
@@ -1,6 +1,26 @@
 package com.hacksu.CommunicationLibrary;
 
+import java.util.List;
+
 public class HttpComm
 {
+    public static List<Product> AllProducts()
+    {
+        return null;
+    }
 
+    public static void UpdateOrder()
+    {
+
+    }
+
+    public static void PlaceOrder()
+    {
+
+    }
+
+    public static void RemoveItem()
+    {
+
+    }
 }
