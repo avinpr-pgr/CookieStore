a1063920a6479fda8e40c106d6f1544036930f17
Mohammad Doleh <mdoleh@kent.edu>
2014-11-03 22:56:44 -0500

    Tabbed interface working

---
+++ AndroidApp/src/com/hacksu/CookieStore/Fragments/CartFragment.java
@@ -0,0 +1,20 @@
+package com.hacksu.CookieStore.Fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.hacksu.CookieStore.R;
+
+public class CartFragment extends Fragment
+{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.cart_layout, container, false);
+
+        return rootView;
+    }
+}
