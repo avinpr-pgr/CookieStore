0055d1fc83c3c49110c2e17c5ef9d88aec71871c
Mohammad Doleh <mdoleh@kent.edu>
2014-11-04 17:00:44 -0500

    Temporary data setup

--- AndroidApp/src/com/hacksu/CookieStore/Fragments/HomeFragment.java
+++ AndroidApp/src/com/hacksu/CookieStore/Fragments/HomeFragment.java
@@ -6,6 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import com.hacksu.CookieStore.R;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class HomeFragment extends Fragment
 {
@@ -17,4 +19,20 @@ public class HomeFragment extends Fragment
 
         return rootView;
     }
+
+    private JSONObject getData()
+    {
+        JSONObject data = null;
+        try
+        {
+            data = new JSONObject(tempData);
+        }
+        catch (JSONException e)
+        {
+            e.printStackTrace();
+        }
+        return data;
+    }
+
+    private String tempData = "{\\r\\n\\\"Products\\\": [\\r\\n{\\r\\n\\\"Category\\\": \\\"Cookie\\\",\\r\\n\\\"Items\\\": [\\r\\n{\\r\\n\\\"id\\\": 1,\\r\\n\\\"description\\\": \\\"Peanut butter\\\",\\r\\n\\\"price\\\": 15\\r\\n},\\r\\n{\\r\\n\\\"id\\\": 2,\\r\\n\\\"description\\\": \\\"Bacon\\\",\\r\\n\\\"price\\\": 5\\r\\n}\\r\\n]\\r\\n},\\r\\n{\\r\\n\\\"Category\\\": \\\"Milk\\\",\\r\\n\\\"Items\\\": [\\r\\n{\\r\\n\\\"id\\\": 3,\\r\\n\\\"description\\\": \\\"Chocolate milk\\\",\\r\\n\\\"price\\\": 15\\r\\n},\\r\\n{\\r\\n\\\"id\\\": 4,\\r\\n\\\"description\\\": \\\"Organic\\\",\\r\\n\\\"price\\\": 15\\r\\n}\\r\\n]\\r\\n}\\r\\n]\\r\\n}";
 }
