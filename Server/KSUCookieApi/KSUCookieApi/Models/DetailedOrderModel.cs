2dadaf66a77cf41e44cf20dbbbeeaabb693f4345
Chris Morton <christopher_morton@hotmail.com>
2014-11-17 08:11:46 -0500

    Implemented DetailedOrderModel to assist Android dev.

---
+++ Server/KSUCookieApi/KSUCookieApi/Models/DetailedOrderModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KSUCookieApi.Models
+{
+    public class DetailedOrderModel :  OrderModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
